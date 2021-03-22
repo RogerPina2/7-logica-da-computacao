0594e295ab4c1093f3bd1de35c77ae21f82f0ac6
RogerPina2 <roger.pina12@hotmail.com>
2021-03-22 17:48:48 -0300

    v1.2 finalizada

--- Token.py
+++ Token.py
@@ -6,6 +6,11 @@ class Token():
         self.type = _type
         self.value = value
 
+def getToken(typeName):
+    for token in tokens:
+        if token.type == typeName:
+            return token
+
 tokens = [
     Token("INT",    re.compile(r'[0-9]+')),
     Token("PLUS",   re.compile(r'\+')),
@@ -13,5 +18,7 @@ tokens = [
     Token("MULT",   re.compile(r'\*')),
     Token("DIV",    re.compile(r'/')),
     Token("SPACE",  re.compile(r' ')),
-    Token("EOF",    re.compile(r'$'))
+    Token("EOF",    re.compile(r'$')),
+    Token("L_PAR",  re.compile(r'\(')),
+    Token("R_PAR",  re.compile(r'\)'))
 ]
